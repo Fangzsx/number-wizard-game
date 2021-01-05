c1bc901f7282478a94cbe2a925fa12d66636cfe3
Fangszx <jygrzn@gmail.com>
2021-01-05 14:40:14 +0800

    Complete
    Added How to play Scene
    Added the number wizard logic.
    Game Over Scene
    Looped

---
+++ Number Wizard Game/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] Text guessText;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+
+    public void OnPressHigher()
+    {
+        //set the minimum value = guess
+        min = guess  + 1 ;
+        //change value of guess
+        NextGuess();
+    }
+
+    public void onPressLower()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+
+
+    void NextGuess()
+    {
+        guess = Random.Range(min,max + 1);
+        guessText.text = ("Is it " + guess + "?");
+
+    }
+}
---
+++ Number Wizard Game/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+
+    
+    public void LoadNextScene()
+    {
+        //index of currentIndex Scene
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+
+    }
+
+    public void LoadStartScreen(){
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadHowToPlayScreen(){
+        SceneManager.LoadScene(3);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
